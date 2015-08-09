9e5420e4a8ccd7c1563841ee4b2cd2f6e3671c92
Lomyal <wangshijun1995@gmail.com>
2015-08-09 20:44:56 +0800

    start using Velocity Template Language

--- src/main/java/cn/edu/pku/sei/dr/StigModController.java
+++ src/main/java/cn/edu/pku/sei/dr/StigModController.java
@@ -1,18 +1,43 @@
 package cn.edu.pku.sei.dr;
 
+//import java.util.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class StigModController {
-    @RequestMapping(method = RequestMethod.GET)
+
+    // Common settings
+    String host = "localhost";
+    String port = "9999";
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
     public String index(ModelMap model) {
-        model.addAttribute("host", "localhost");
-        model.addAttribute("port", "9999");
-//        model.addAttribute("title", "SM");
+        model.addAttribute("host", host);
+        model.addAttribute("port", port);
+        model.addAttribute("title", "index");
+
+//        List<Integer> allProducts = new ArrayList<Integer>();
+//        allProducts.addAll(Arrays.asList(4, 5, 6));
+//        System.out.println(allProducts);
+//        model.addAttribute("allProducts", allProducts);
+//
+//        List<String> customerList = new ArrayList<String>();
+//        customerList.addAll(Arrays.asList("Zhang San", "Li Si", "Wang Wu"));
+//        System.out.println(customerList);
+//        model.addAttribute("customerList", customerList);
+
         return "index";
+    }
+
+    @RequestMapping(value="/reg", method = RequestMethod.GET)
+    public String reg(ModelMap model) {
+        model.addAttribute("host", host);
+        model.addAttribute("port", port);
+        model.addAttribute("title", "Sign Up");
+        return "reg";
     }
 }
