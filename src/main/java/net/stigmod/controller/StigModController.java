692b469f4337a742574d0674c021876b37cab05a
Lomyal <wangshijun1995@gmail.com>
2015-11-10 21:07:17 +0800

    add config.xml to gitignore

--- src/main/java/net/stigmod/controller/StigModController.java
+++ src/main/java/net/stigmod/controller/StigModController.java
@@ -1,7 +1,7 @@
 /*
- * Copyright 2014-2016, Stigmergic-Modeling Project,
- * SEIDR, Peking University,
- * All rights reserved.
+ * Copyright 2014-2016, Stigmergic-Modeling Project
+ * SEIDR, Peking University
+ * All rights reserved
  *
  * Stigmergic-Modeling is used for collaborative groups to create a conceptual model.
  * It is based on UML 2.0 class diagram specifications and stigmergy theory.
