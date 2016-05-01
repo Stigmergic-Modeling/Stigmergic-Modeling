a9e873f9c9b12420d7f7665d2887083aec136595
Lomyal <wangshijun1995@gmail.com>
2016-05-02 03:04:59 +0800

    fix a dup signing up bug

---
+++ src/main/javascript/src/js/app/signup.js
@@ -0,0 +1,24 @@
+define(function (require, exports, module) {
+
+    var $ = require('../lib/jquery');
+    require('../lib/bootstrap');
+
+    // 点击注册后失能注册按钮一段时间，防止快速重复点击引发 bug
+    $(document).on('click', '#stigmod-fake-submit-1', function() {
+
+        var $fakeSubmit1 = $('#stigmod-fake-submit-1');
+        var $fakeSubmit2 = $('#stigmod-fake-submit-2');
+        var $realSubmit = $('#stigmod-real-submit');
+
+        $fakeSubmit1.hide();
+        $fakeSubmit2.show();
+
+        setTimeout(function() {
+            $fakeSubmit2.hide();
+            $fakeSubmit1.show();
+        }, 10000);
+
+        $realSubmit.trigger('click');
+    });
+
+});
