ce6f34cfca8e90a0433b1646d9ad8af6d61f7433
Lomyal <wangshijun1995@gmail.com>
2016-06-13 11:01:09 +0800

    change autosave interval

--- src/main/javascript/src/js/module/page.js
+++ src/main/javascript/src/js/module/page.js
@@ -89,7 +89,8 @@ define(function (require, exports, module) {
         });
 
         // 每隔 XX s 进行一次自动保存
-        var saveInterval = 120000;  // 每 120s 进行一次自动保存
+        var saveInterval = 60000;  // 每 60s 进行一次自动保存
+        //var saveInterval = 120000;  // 每 120s 进行一次自动保存
         //var saveInterval = 60000000;  // 调试时，不自动保存（1000分钟内）
         setInterval(function() {
             var noActiveTime = new Date().getTime() - page.lastActionTime;
